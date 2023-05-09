5f9d8c242e44ecadb0d9687c25fc3aab277f6f43
SurajPrakash <surajprakashhh04@gmail.com>
2023-05-09 10:09:42 +0530

    Handling error with empty submission

--- app.js
+++ app.js
@@ -7,46 +7,60 @@ require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var query="";
-var temp="";
-var des="Enter Your City Name and Click the Search button";
-var iconUrl="";
-var minTemp="";
-var maxTemp="";
-var feelsLike="";
-var pressure="";
-var humidity="";
-var wind="";
+var query = "";
+var temp = "";
+var des = "Enter Your City Name and Click the Search button";
+var iconUrl = "";
+var minTemp = "";
+var maxTemp = "";
+var feelsLike = "";
+var pressure = "";
+var humidity = "";
+var wind = "";
 
-app.get("/", function(req, res){
-    res.render("index", {city:query, ph:iconUrl, tem:temp, des:des, mint:minTemp, maxt:maxTemp, flike:feelsLike, press:pressure, hum:humidity, wspeed:wind});
+app.get("/", function (req, res) {
+    res.render("index", { city: query, ph: iconUrl, tem: temp, des: des, mint: minTemp, maxt: maxTemp, flike: feelsLike, press: pressure, hum: humidity, wspeed: wind });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     query = req.body.cityname;
-    const apikey = process.env.APIKey;
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apikey+"&units="+unit;
-    https.get(url, function(response){
-        // console.log(response.statusCode);
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            temp = weatherData.main.temp+" °C";
-            des = weatherData.weather[0].description;
-            let icon = weatherData.weather[0].icon;
-            iconUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-            minTemp = weatherData.main.temp_min+" °C";
-            maxTemp = weatherData.main.temp_max+" °C";
-            feelsLike = weatherData.main.feels_like+" °C";
-            pressure = weatherData.main.pressure+" hPa";
-            humidity = weatherData.main.humidity+" %";
-            wind = weatherData.wind.speed+" m/s";
-            res.redirect("/");
+    if (query == "") {
+        query = "";
+        temp = "";
+        des = ">> Please enter Your City Name";
+        iconUrl = "";
+        minTemp = "";
+        maxTemp = "";
+        feelsLike = "";
+        pressure = "";
+        humidity = "";
+        wind = "";
+        res.redirect("/");
+    } else {
+        const apikey = process.env.APIKey;
+        const unit = "metric";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
+        https.get(url, function (response) {
+            // console.log(response.statusCode);
+            response.on("data", function (data) {
+                const weatherData = JSON.parse(data);
+                temp = weatherData.main.temp + " °C";
+                des = weatherData.weather[0].description;
+                let icon = weatherData.weather[0].icon;
+                iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                minTemp = weatherData.main.temp_min + " °C";
+                maxTemp = weatherData.main.temp_max + " °C";
+                feelsLike = weatherData.main.feels_like + " °C";
+                pressure = weatherData.main.pressure + " hPa";
+                humidity = weatherData.main.humidity + " %";
+                wind = weatherData.wind.speed + " m/s";
+                res.redirect("/");
+            })
         })
-    })
+    }
 });
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started at port 3000");
 });
